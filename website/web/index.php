e2a456e093f5fda02e651625fbd5b50528897ac2
Christian <christian@eerstelinks.nl>
2013-12-07 19:30:21 +0000

    Snailsnapp Website
    First release preview website

---
+++ website/web/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Snailsnapp</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- nice favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+
+    <!-- Bootstrap -->
+    <link href="assets/bootstrap-3.0.3/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- stylesheets -->
+    <link href="css/start.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="iphone-green">
+          <iframe src="http://127.0.0.1:8020/index.html" ></iframe>
+        </div>
+        <h1 class="header">Snailsnapp is coming!</h1>
+      </div>
+    </div>
+    <!-- Google analytics tracker -->
+    <script src="js/analytics.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/bootstrap-3.0.3/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
