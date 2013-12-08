f77f3123c070e4ca29060abfc651cc22eb938d2c
Adriaan van Rossum <github@adriaanvanrossum.nl>
2013-12-08 14:08:08 +0000

    Advise user to invite friend from server side

--- website/web/api/post/invite.php
+++ website/web/api/post/invite.php
@@ -4,12 +4,19 @@ require(dirname(__FILE__).'/../assets/json_header.php');
 $return['status']  = 'error';
 
 if (count($_POST) == 0) {
-  $return['message'] = 'Nothing received from the app, try again later';
+  $return['message'] = 'The server has nothing received from the app, try again later '.print_r($_POST, true).print_r($_GET, true);
   die(json_encode($return));
 }
-
-$return['status']  = 'success';
-echo json_encode($return);
+elseif (empty($_POST['email_friend'])) {
+  $return['status'] = 'success';
+  $return['message'] = 'Stay tuned, you will get your invite soon! You still can invite a friend, but you can\'t change your own email';
+  echo json_encode($return);
+}
+else {
+  $return['status'] = 'success';
+  $return['message'] = 'Stay tuned, you will get your invite soon!';
+  echo json_encode($return);
+}
 
 // send the data to our email
 $extraInfo['added'] = date('Y-m-d H:i:s');
