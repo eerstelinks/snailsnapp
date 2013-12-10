e173fb8e214198a576cf32bd583ab2a20ffa61e8
Christian <christian@eerstelinks.nl>
2013-12-10 00:37:21 +0000

    Create loginscreen + facebook-button

--- app/2.0/app/alloy.js
+++ app/2.0/app/alloy.js
@@ -1,4 +1,7 @@
-// colors
+// Colors
 Alloy.CFG.green = "#18DB6E";
 Alloy.CFG.grey  = "#DCDCDC";
 Alloy.CFG.white = "#FFFFFF";
+
+// Loads the Facebook module
+Alloy.Globals.Facebook = require('facebook');
--- app/2.0/app/controllers/index.js
+++ app/2.0/app/controllers/index.js
@@ -1,1 +1,29 @@
+// Alerts
+function showErrorAlert(message, button, title) {
+	dialog = Ti.UI.createAlertDialog({
+    	message: message || 'Something went wrong',
+    	ok: button || 'Let me try again',
+    	title: title || 'Snapp...'
+	}).show();
+}
+
+// Alloy required for Facebook
+var facebook = Alloy.Globals.Facebook;
+facebook.appid = "1417460311823818"; // Facebook App ID
+facebook.permissions = ["email", "user_birthday", "user_location", "publish_actions"]; // Facebook App Permissions
+
+// Hide fbButton after login
+facebook.addEventListener('login', function(e) {
+    if (e.success) {
+		$.fbButton.hide();
+    }
+    else {
+    	showErrorAlert('Something went wrong');
+    }
+});
+
+$.fbButton.style = facebook.BUTTON_STYLE_WIDE; // Shows 'Connect to Facebook' instead of 'Connect'
+
 $.index.open();
+
+
