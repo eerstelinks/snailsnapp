d7db430977c274d873dc1696a3c26f07821fb405
Christian <christian@eerstelinks.nl>
2013-12-11 13:45:42 +0000

    Add postphoto window
    This is bleeding edge! Will be edited soon…

---
+++ app/2.0/app/controllers/postphoto.js
@@ -0,0 +1,9 @@
+function outputStateFacebook() {
+    Ti.API.info('Switch value: ' + $.postFacebook.value);
+}
+function outputStatePublic() {
+    Ti.API.info('Switch value: ' + $.postPublic.value);
+}
+function outputStateAnonymous() {
+    Ti.API.info('Switch value: ' + $.postAnonymous.value);
+}
