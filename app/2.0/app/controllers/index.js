0262baf280a3b2dcde08b3bf33eaee6ce5fcb598
Adriaan van Rossum <github@adriaanvanrossum.nl>
2013-12-11 14:32:18 +0000

    Rename files so the map will be the first window

--- app/2.0/app/controllers/index.js
+++ app/2.0/app/controllers/index.js
@@ -1,16 +1,69 @@
-// Alloy required for Facebook
-var facebook = Alloy.Globals.Facebook;
-facebook.appid = "1417460311823818"; // Facebook App ID
-facebook.permissions = ["email", "user_birthday", "user_location", "publish_actions"]; // Facebook App Permissions
+$.mapview.showsPointsOfInterest = false;
+$.mapview.userLocationButton = false;
+$.mapview.traffic = false;
 
-// Hide fbButton after login
-facebook.addEventListener('login', function(e) {
-    if (e.success) {
-		Alloy.createController('map').getView().open();
-    }
-    else {
-    	showErrorAlert();
-    }
+$.mapview.userLocation = true;
+
+$.mapview.region = {
+	latitude: 52.373056,
+	longitude: 4.892222,
+	latitudeDelta: 0.1,
+	longitudeDelta: 0.1
+};
+
+function setLocation(coords){
+    var region = {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        animate: true,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+    };
+    $.mapview.setLocation(region);
+}
+
+function locateMe() {
+	if (Ti.Geolocation.locationServicesEnabled) {
+	    Ti.Geolocation.purpose = L('geo_own_location_purpose', 'Get Current Location');
+	    Ti.Geolocation.accuracy = Ti.Geolocation.ACCURACY_BEST;
+	    Ti.Geolocation.distanceFilter = 10;
+	    Ti.Geolocation.preferredProvider = Ti.Geolocation.PROVIDER_GPS;
+
+	    Ti.Geolocation.getCurrentPosition(function(e) {
+	        if (e.error) {
+	    		showErrorAlert(L('geo_no_location_message'), L('geo_no_location_button'));
+	        } else {
+	            setLocation(e.coords);
+	        }
+	    });
+	}
+}
+
+Alloy.Globals.Facebook.addEventListener('logout', function(e) {
+    Alloy.createController('login').getView().open();
 });
+
+
+function fbLogOut() {
+	Alloy.Globals.Facebook.logout();
+}
+
+locateMe();
+
+//trigger camera after hitting the snailHome
+function snailHome() {
+    Ti.Media.showCamera({
+        success:function(e) {
+	       	Alloy.createController('postphoto').getView().open();
+        },
+        error:function(e) {
+            showErrorAlert(L('camera_error_message'));
+        },
+        allowEditing:false,
+        saveToPhotoGallery:true,
+        //overlay:squareOverlay,
+        mediaTypes:[Ti.Media.MEDIA_TYPE_PHOTO]
+    });
+}
 
 $.index.open();
---
+++ app/2.0/app/controllers/login.js
@@ -0,0 +1,14 @@
+// Alloy required for Facebook
+var facebook = Alloy.Globals.Facebook;
+facebook.appid = "1417460311823818"; // Facebook App ID
+facebook.permissions = ["email", "user_birthday", "user_location", "publish_actions"]; // Facebook App Permissions
+
+// Hide fbButton after login
+facebook.addEventListener('login', function(e) {
+    if (e.success) {
+		  $.login.close();
+    }
+    else {
+    	showErrorAlert();
+    }
+});
