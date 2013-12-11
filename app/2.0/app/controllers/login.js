5c3cae2c926afc9a287dbe3e0f62a3f766d32e50
Adriaan van Rossum <github@adriaanvanrossum.nl>
2013-12-11 20:44:48 +0000

    Send user to login screen when not logged in

--- app/2.0/app/controllers/login.js
+++ app/2.0/app/controllers/login.js
@@ -1,6 +1,7 @@
 // Hide fbButton after login
 facebook.addEventListener('login', function(e) {
   if (e.success) {
+    Alloy.createController('index').getView().open();
     $.login.close();
   }
   else {
