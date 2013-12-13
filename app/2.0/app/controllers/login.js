29507dd466ccd4a5249c835346b916835c41b6b4
Adriaan van Rossum <github@adriaanvanrossum.nl>
2013-12-13 17:21:49 +0000

    Show loading indicator after facebook login

--- app/2.0/app/controllers/login.js
+++ app/2.0/app/controllers/login.js
@@ -1,31 +1,45 @@
 // is the map already loaded?
 var showedMap = Ti.App.Properties.getBool('showed_map', false);
 
-// hide fbButton after login
-$.fbLoader.hide();
+// show skip button when user has not skipped before
+if (!Ti.App.Properties.getBool('has_skipped_login', true)) {
+  $.skipLogin.show();
+}
+else {
+  $.skipLogin.text = $.skipLogin.getText() + ' (only for dev)';
+  $.skipLogin.show();
+}
 
 facebook.addEventListener('login', function(e) {
   if (e.success) {
 
-    // show indicator of loading
+    // hide logout button
     $.fbButton.hide();
+
+    // hide skip, because you will get there sowieso
+    $.skipLogin.hide();
+
+    // show indicator of loading
     $.fbLoader.show();
 
     if (!showedMap) {
       Alloy.createController('index').getView().open();
     }
 
-    $.login.close();
+    // set timeout so the indicater will show
+    setTimeout(function(){
+      $.login.close();
+    }, 1500);
+
   }
   else {
-    showErrorAlert();
+    showErrorAlert(e.error);
   }
 });
 
-//skip login if asked to do so, then hide the button
+//skip login if asked to do so
 function goHome() {
-  Ti.App.Properties.setBool('has_skipped_login' , true);
-
+  Ti.App.Properties.setBool('has_skipped_login', true);
 
   if (!showedMap) {
     Alloy.createController('index').getView().open();
