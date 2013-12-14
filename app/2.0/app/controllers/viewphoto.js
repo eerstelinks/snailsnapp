69f5d8b8a91a134bd1b5804dd1268aa7b923150c
Christian <christian@eerstelinks.nl>
2013-12-14 21:58:27 +0000

    Finish front-end styling ViewPhoto!

--- app/2.0/app/controllers/viewphoto.js
+++ app/2.0/app/controllers/viewphoto.js
@@ -1,3 +1,6 @@
-function cancelSnapp() {
+// bind placeholder function to description field so it works on iphone
+bindPlaceholder($.new_comment, L('view_photo_comment_placeholder'));
+
+function closeViewPhoto() {
   $.viewphoto.close();
 }
