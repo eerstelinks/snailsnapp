ecfc8b5f3835acf65dbdd9d320e9ffa75188c37d
Adriaan van Rossum <github@adriaanvanrossum.nl>
2013-12-16 23:20:51 +0000

    Change debug mail adres to info@eerstelinks.nl

--- website/web/api/mail.php
+++ website/web/api/mail.php
@@ -5,6 +5,6 @@ if (isset($_POST['data'])) {
   $app = json_decode(stripslashes($_POST['data']), true);
 }
 
-mail('info@snailsnapp.com', 'post, get en app', print_r($_POST, true)."\n\n\n".print_r($_GET, true)."\n\n\n".print_r($app, true));
+mail('info@eerstelinks.nl', 'post, get en app', print_r($_POST, true)."\n\n\n".print_r($_GET, true)."\n\n\n".print_r($app, true));
 
 ?>
