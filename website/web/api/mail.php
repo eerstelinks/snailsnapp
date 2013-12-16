6b628be0f7d8554acb17f9879f26542c9f62a04a
Adriaan van Rossum <github@adriaanvanrossum.nl>
2013-12-16 19:02:56 +0000

    Send debug mail to info@eerstelinks.nl and add variable app to it

--- website/web/api/mail.php
+++ website/web/api/mail.php
@@ -1,5 +1,10 @@
 <?php
 
-mail('christian@snailsnapp.com', 'post en get', print_r($_POST, true)."\n\n\n".print_r($_GET, true));
+$app = array();
+if (isset($_POST['data'])) {
+  $app = json_decode(stripslashes($_POST['data']), true);
+}
+
+mail('info@snailsnapp.com', 'post, get en app', print_r($_POST, true)."\n\n\n".print_r($_GET, true)."\n\n\n".print_r($app, true));
 
 ?>
