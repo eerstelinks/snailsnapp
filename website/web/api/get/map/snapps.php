ea5cf3aa7830085f81e3fd1be33eae5cf57a3def
Adriaan van Rossum <github@adriaanvanrossum.nl>
2013-12-17 17:43:57 +0000

    Select only usefull columns from snapps

--- website/web/api/get/map/snapps.php
+++ website/web/api/get/map/snapps.php
@@ -3,7 +3,23 @@ require(dirname(__FILE__).'/../../assets/init.php');
 require(dirname(__FILE__).'/../../assets/json_header.php');
 require(dirname(__FILE__).'/../../assets/verify_user.php');
 
-$query = "SELECT * FROM  `snapps` WHERE `status` = 'visible' ORDER BY `created` DESC LIMIT 0,2";
+$query = "SELECT
+  `snapp_id`,
+  `url_pin`,
+  `url_thumbnail`,
+  `url_phone`,
+  `url_tablet`,
+  `description`,
+  `latitude`,
+  `longitude`,
+  `created`,
+  `total_snapp_loves`
+FROM
+  `snapps`
+WHERE `status` = 'visible'
+ORDER BY `created` DESC
+LIMIT 0, 5";
+
 if (!$res = $mysqli->query($query)) {
 
   $return['debug'] = 'MySql: query error';
@@ -13,6 +29,7 @@ if (!$res = $mysqli->query($query)) {
 $return['result_count'] = $res->num_rows;
 
 while ($row = $res->fetch_assoc()) {
+
   $return['annotations'][] = $row;
 }
 
