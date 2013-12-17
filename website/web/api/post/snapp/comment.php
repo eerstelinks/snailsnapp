2cbf9f85990e127fb9cb6749f8e58bf621b3e31f
Christian <christian@eerstelinks.nl>
2013-12-17 00:34:02 +0000

    Add user_id + improve script post snapp comment

--- website/web/api/post/snapp/comment.php
+++ website/web/api/post/snapp/comment.php
@@ -1,78 +1,30 @@
 <?php
 require(dirname(__FILE__).'/../../assets/init.php');
 require(dirname(__FILE__).'/../../assets/json_header.php');
+require(dirname(__FILE__).'/../../assets/verify_user.php');
 
-$return['status'] = 'error';
-
-if (isset($_POST['data'])) {
-  $app = json_decode(stripslashes($_POST['data']), true);
-}
-else {
-  $return['debug'] = 'No post data';
-  die(json_encode($return));
-}
-
-// check data from the app
-if (empty($app['always']['facebook'])) {
-  $return['debug']  = 'no data';
-  die(json_encode($return));
-}
-
-// required items
-$requiredItems = array(
-  $app['always']['facebook']['user_id'],
-  $app['always']['facebook']['access_token'],
-  $app['always']['facebook']['expiration_date'],
-  $app['snapp_id'],
-  $app['comment']
-);
-
-// check for required items
-foreach ($requiredItems as $key => $item) {
-  if (empty($item)) {
-    $return['debug'] = 'no '.$key; // THIS RETURNS A NUMBER, ADRIAAN?
-    die(json_encode($return));
-  }
-}
-
-if (!empty($error)) {
-  $return['debug']  = implode(', ', $error);
+if (empty($app['snapp_id']) || empty($app['comment'])) {
+  $return['debug'] = 'no snapp_id or comment';
   die(json_encode($return));
 }
 
 unset($insert);
-$checkKeys = array(
-  'created'           => 'created'
-);
-
-foreach ($checkKeys as $databaseKey => $appKey) {
-  if (!empty($app[$appKey])) {
-    $insert[$databaseKey] = $app[$appKey];
-  }
-  // if created is not received from app, use current datetime UTC
-  else if ($databaseKey = 'created' && empty($app['created'])) {
-    $app['created']    = date("Y-m-d H:i:s");
-    $insert['created'] = $app['created'];
-  }
-}
-
-$insert['snapp_id'] = $app['snapp_id'];
-$insert['comment']  = $app['comment'];
-$insert['ss_user_id']  = $ss_user_id; // SHOULD ALWAYS BE THERE
+$insert['snapp_id']   = $app['snapp_id'];
+$insert['comment']    = $app['comment'];
+$insert['ss_user_id'] = $ss_user_id;
 
 // check if the record already exists
-$checkExisting = $insert;
-unset($checkExisting['created']);
-$query  = "SELECT `snapp_comment_id` FROM `snapp_comments` WHERE ".cf_implode_mysqli($checkExisting, null, ' AND ');
+$query  = "SELECT `snapp_comment_id` FROM `snapp_comments` WHERE ".cf_implode_mysqli($insert, null, ' AND ');
 $result = $mysqli->query($query);
 
 if ($result->num_rows > 0) {
-  $return['debug']  = 'MySql: record already exists!';
+  $return['message'] = 'Bericht bestaat al, je kan niet twee keer precies hetzelfde reageren';
+  $return['debug']   = 'MySql: record already exists!';
   die(json_encode($return));
 }
 
 // all ok, create a database entry for the new comment!
-$query = "INSERT INTO `snapp_comments` SET ".cf_implode_mysqli($insert)."";
+$query = "INSERT INTO `snapp_comments` SET ".cf_implode_mysqli($insert).", `created` = NOW()";
 
 if ($mysqli->query($query)) {
 
