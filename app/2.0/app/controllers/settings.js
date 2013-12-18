2fbd370a87dc5525ddcf76258b57ad65a743161b
Christian <christian@eerstelinks.nl>
2013-12-18 19:40:47 +0000

    Post notification preferences to correct path

--- app/2.0/app/controllers/settings.js
+++ app/2.0/app/controllers/settings.js
@@ -24,7 +24,7 @@ function toggleSwitch(event) {
 
 function sendSettingsToSnailSnapp() {
   uploadToSnailsnapp(
-    '/mail',
+    '/post/user/notifications',
     function() {
       showSuccessAlert();
     },
