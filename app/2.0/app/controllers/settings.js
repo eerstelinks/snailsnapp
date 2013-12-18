6f1ae71ffd0e8586d7697611727490c27b1e04d9
Christian <christian@eerstelinks.nl>
2013-12-18 16:55:52 +0000

    Front end settings screen

--- app/2.0/app/controllers/settings.js
+++ app/2.0/app/controllers/settings.js
@@ -1,3 +1,22 @@
 function closeSettings() {
   $.settings.close();
 }
+
+function toggleSwitch(event) {
+  var toggle         = event.source;
+  var toggleId       = toggle.id;
+  var toggleValue    = toggle.value;
+
+  if (toggleId == 'never' && toggleValue === true) {
+    $.loves_my_snapp.setValue(false);
+    $.loves_my_comment.setValue(false);
+    $.comments_my_snapp.setValue(false);
+    $.comments_my_comment.setValue(false);
+    $.loves_comment_my_snapp.setValue(false);
+    $.loves_comment_my_comment.setValue(false);
+    //return;
+  }
+  else if ($.never.value === true && toggleValue === true) {
+    $.never.setValue(false);
+  }
+}
