5e4065eb77f458ce8c7b37d0d26faa6e465cfd42
Chrissvo <christian@Christians-iMac.local>
2014-01-03 23:46:43 +0100

    Major update: Insert comments, slide in info, set dates right, etc.

--- website/web/js/map.js
+++ website/web/js/map.js
@@ -1,5 +1,11 @@
 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDl9MCsn7lVAV69CkVvvo6hDg2YDV1ScV0&sensor=true"
 
+function showLocaleDate(utc) {
+  var date = new Date(utc);
+  var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+  return date.getDate() + ' ' + monthNames [date.getMonth()] + ' ' + date.getFullYear() + ' ' + date.getHours() + ':' + (date.getMinutes() < 10?'0':'') + date.getMinutes();
+}
+
 var mapOptions = {
   zoom: 8
 };
@@ -10,7 +16,7 @@ function initialize() {
 
   var snapps = [];
 
-  // Try HTML5 geolocation
+  // try HTML5 geolocation
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -35,7 +41,6 @@ function initialize() {
 
 function handleNoGeolocation() {
   // zoom to Amsterdam on a higher zoomlevel
-  console.log('ii');
   var options = {
     map: map,
     position: new google.maps.LatLng(52.373056, 4.892222),
@@ -60,27 +65,99 @@ function setMarkers(map, snapps) {
   var map_longitude       = SW.ob;
   var map_longitude_delta = (NE.ob - SW.ob);
   var map_type            = 'public';
+  var markers              = [];
 
   // get the snapps
-  $.getJSON( "/api/get/map/snapps?latitude=" + map_latitude + "&latitude_delta=" + map_latitude_delta + "&longitude=" + map_longitude + "&longitude_delta= " + map_longitude_delta + "&type=" + map_type, function(snapps) {
+  $.getJSON("/api/get/map/snapps?latitude=" + map_latitude + "&latitude_delta=" + map_latitude_delta + "&longitude=" + map_longitude + "&longitude_delta= " + map_longitude_delta + "&type=" + map_type, function(snapps) {
     if (snapps.result_count > 0) {
-      for (var i = 0; i < snapps.result_count; i++) {
+      for (i = 0; i < snapps.result_count; i++) {
 
-        var snapp           = snapps.annotations[i];
-        var snapp_marker    = snapp.url_pin;
+        // source: http://stackoverflow.com/a/13560751
+        (function (i) {
 
-        var snapp_latLng    = new google.maps.LatLng(snapp.latitude, snapp.longitude);
-        var marker          = new RichMarker({
-          position: snapp_latLng,
-          map:      map,
-          shadow:   0,
-          content:  '<img class="snapp_marker" src="' + snapp_marker + '" />'
-        });
+          // build the beautiful marker
+          var snapp           = snapps.annotations[i];
+          var snapp_marker    = snapp.url_pin;
+          var snapp_latLng    = new google.maps.LatLng(snapp.latitude, snapp.longitude);
+          var marker          = new RichMarker({
+            snapp:    snapp,
+            position: snapp_latLng,
+            map:      map,
+            shadow:   0,
+            content:  '<img class="snapp_marker" src="' + snapp_marker + '" />'
+          });
+
+          // listen for click event
+          google.maps.event.addListener(marker, 'click', function() {
+            viewSnapp(marker);
+          });
+
+        }) (i);
       }
     }
-    else {
+  });
+}
+
+function viewSnapp(marker) {
+  // display snapp information
+  $('#fb_profile_pic').attr('src', 'http://graph.facebook.com/' + marker.snapp.fb_user_id + '/picture?width=120&height=120');
+  $('#fb_full_name').html(marker.snapp.fb_full_name);
+  $('#snapp_created').html(showLocaleDate(marker.snapp.created));;
+  $('#snapp_description').html(marker.snapp.description);
+  // display the snapp itself
+  $('#snapp_image').attr('src', marker.snapp.url_tablet);
+  // display total loves
+  if (marker.snapp.total_snapp_loves > 0) {
+    $('#image_love').html('<i class="fa fa-heart"></i> ' + marker.snapp.total_snapp_loves + ' x');
+  }
+  else {
+    $('#image_love').html('<i class="fa fa-heart-o"></i> 0 x');
+  }
+  // get the comments
+  $.getJSON("/api/get/snapp/metadata?snapp_id=" + marker.snapp.snapp_id, function(metadata) {
+
+    // empty the comment_wrapper first to remove old data
+    $('#comment_wrapper').html('');
+    if (metadata.comments) {
+      for (i= 0; i < metadata.result_count; i++) {
+        var comment      = metadata.comments[i];
+        var comment_love = '<i class="fa fa-heart-o"</i> 0 x';
+        if (comment.total_comment_loves > 0) {
+          comment_love = '<i class="fa fa-heart"></i> ' + comment.total_comment_loves + ' x';
+        }
+        var datetime     = showLocaleDate(comment.created);
+        // fill with the new comments
+        $('#comment_wrapper').append('<div class="snapp_info">' +
+          '<img id="fb_profile_pic" src="http://graph.facebook.com/' + comment.fb_user_id + '/picture?width=120&height=120" />' +
+          '<div class="snapp_details">' +
+            '<p id="fb_full_name">' + comment.fb_full_name + '</p>' +
+            '<p id="snapp_created">' + datetime + '</p>' +
+            '<p id="snapp_description">' + comment.comment + '</p>' +
+            '<p id="comment_love">' + comment_love + '</p>' +
+          '</div>' +
+        '</div>');
+      }
     }
   });
+  // show snapp container
+  showViewSnapp();
+  //$('#snapp_container').show();
+}
+
+$('#close_container').on('click', function(){
+  closeViewSnapp();
+});
+
+function showViewSnapp() {
+  var containerWidth = '300px';
+  if ($('#snapp_container').width() == 0) {
+    $('#snapp_container').animate({width: containerWidth}, 'slow', function() {$('#close_container').show()});
+  }
+}
+
+function closeViewSnapp() {
+  $('div#snapp_container').animate({width: 0}, 'slow');
+  $('#close_container').hide();
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
--- website/web/map.php
+++ website/web/map.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
+    <!-- font awesome -->
+    <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+    <!-- css -->
     <link href="style/css/map.css" rel="stylesheet" />
+    <!-- required for Google Maps -->
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 
     <!-- Google Maps API Key -->
@@ -12,8 +18,26 @@
 
   <body>
 
-    <div id="map-canvas" />
+    <div id="map-canvas"></div>
+    <div id="close_container"><i class="fa fa-times-circle fa-lg"></i></div>
+    <div id="snapp_container">
+      <div class="snapp_info">
+        <img id="fb_profile_pic" />
+        <div class="snapp_details">
+          <p id="fb_full_name"></p>
+          <p id="snapp_created"></p>
+          <p id="snapp_description"></p>
+        </div>
+      </div>
+      <div class="snapp_image">
+        <img id="snapp_image" />
+        <p id="image_love"></p>
+      </div>
 
+      <!-- IF COMMENTS ARE AVAILABLE PLACE THEM HERE -->
+      <div id="comment_wrapper">
+      </div>
+    </div>
     <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="js/map.js"></script>
     <script src="js/richmarker.js"></script>
